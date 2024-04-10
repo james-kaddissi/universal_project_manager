1510ce6d85c03bd2c7103d739e6d00a5cb031383
ANONyMOSy <jameskaddissi@gmail.com>
2024-04-10 10:51:09 -0400

    On unix, the PATH is automatically updated to include 'upm' instead of having to manually do it

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
-// build.rs
+use std::process::Command;
+
 fn main() {
     #[cfg(target_os = "windows")]
     windows_instructions();
@@ -16,6 +17,16 @@ fn windows_instructions() {
 }
 
 fn unix_instructions() {
-    println!("cargo:warning=To add the program to your PATH on Unix, run the following commands:");
-    println!("cargo:warning=sudo cp target/release/ultimate_project_manager /usr/local/bin/upm");
+    if cfg!(target_os = "macos") || cfg!(target_os = "unix") {
+        // Attempt to copy the binary using `sudo`, which might prompt the user for their password.
+        match Command::new("sudo")
+            .arg("cp")
+            .arg("target/release/ultimate_project_manager")
+            .arg("/usr/local/bin/upm")
+            .status()
+        {
+            Ok(status) if status.success() => println!("cargo:warning=Successfully copied the binary to /usr/local/bin."),
+            _ => println!("cargo:warning=Failed to copy the binary. Please manually copy it to a directory in your PATH."),
+        }
+    }
 }
